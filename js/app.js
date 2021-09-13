c3a92c98f6a3c2235c8fd5797c66e97c18812261
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-09-13 10:52:23 +0600

    total price update fixed

--- js/app.js
+++ js/app.js
@@ -29,8 +29,8 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
