957f9e62431f487819b6f87bed3218693bbd1583
Md Amdadul Haq Arif <79464808+amdadulgfx@users.noreply.github.com>
2021-09-13 10:46:35 +0600

    row fixed,total item fixed , image shown fixed

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -77,4 +77,5 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+
 loadProducts();
